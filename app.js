e37b7f4e19e2ee385b4e64a470ca65c11972d455
Vandana Kumari <vandana.kumari@semapp.de>
2021-02-01 09:47:25 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,51 @@
+//Movement animation to occur
+const container = document.querySelector(".container");
+const card = document.querySelector(".card");
+
+//Adding animation to rest  of the items
+const earthImg = document.querySelector(".Earth img");
+const description = document.querySelector(".info h3"); 
+const title = document.querySelector(".title");
+const planetsButton = document.querySelector(".planets button");
+const planetsImg = document.querySelector(".planets img");
+const habitat = document.querySelector(".habitat");
+
+
+//Moving animation events
+container.addEventListener("mousemove", (eve) => {
+    //console.log(eve.pageX, eve.pageY);
+    let xAxix = (window.innerWidth / 2 - eve.pageX) / 40;
+    let yAxix = (window.innerHeight / 2 - eve.pageY) / 300;
+
+    card.style.transform =`rotateY(${xAxix}deg) rotateX(${yAxix}deg)`;
+});
+
+
+//Animate In
+container.addEventListener("mouseenter", (eve) => {
+card.style.transition = "none";
+
+//Popout
+title.style.transform = "translateZ(150px)";
+earthImg.style.transform = "translateZ(200px) rotate(-50deg)";
+description.style.transform = "translateZ(125px)";
+planetsButton.style.transform = "translateZ(100px)";
+planetsImg.style.transform = "translateZ(75px) rotate(-5deg)";
+habitat.style.transform = "translateZ(50px)";
+
+});
+
+//Animate Out
+container.addEventListener("mouseleave", (eve) => {
+card.style.transition = "all 0.5s ease";
+card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+
+//Popback
+title.style.transform = "translateZ(150px)";
+earthImg.style.transform = "translateZ(0px) rotate(0deg)";
+description.style.transform = "translateZ(0px)";
+planetsButton.style.transform = "translateZ(0px)";
+planetsImg.style.transform = "translateZ(0px) rotate(0deg)";
+habitat.style.transform = "translateZ(0px)";
+
+});
